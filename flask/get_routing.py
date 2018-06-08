d50deec9e62d91e0ebdbb588a538349ce0386882
katisari <39178730+katisari@users.noreply.github.com>
2018-06-08 07:45:10 -0700

    Create get_routing.py

---
+++ flask/get_routing.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+app=Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World"
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+@app.route('/say/<name>')
+def say(name):
+    return "Hi " + name.title()
+
+@app.route('/repeat/<num>/<word>')
+def repeat(num, word):
+    return (word + "\n") * int(num)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
